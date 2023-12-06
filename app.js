ea6938092941dfadb719b74a1d54bfcd3192f123
indrabsudirman <indrabsudirman@gmail.com>
2023-12-06 08:19:02 +0700

    Add sample simple Get and Post

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app = express();
 const PORT = 3001;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello from the server side!");
+  res.status(200).json(
+    { message: "Hello from the server side!", app: 'Learn Xpress' });
+});
+
+app.post('/', (req, res) => {
+  res.status(200).send("You can Post with this endpoint ...");
 });
 
 app.listen(PORT, () => {
