b450c581a27bd0ce60b9759064b23a381491d9a6
indrabsudirman <indrabsudirman@gmail.com>
2023-12-09 14:51:10 +0700

    Add connections to MongoDB Local or Cloud

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
+
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+//Connect to the cloud
+// const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+// mongoose.connect(DB).then(
+//   (con) => {
+//     console.log(con.connections);
+//     console.log('Success connected!');
+//   },
+//   (err) => {
+//     console.log(err);
+//   },
+// );
+
+//Connect to the local
+mongoose.connect(process.env.DB_LOCAL).then(
+  (con) => {
+    console.log(con.connections);
+    console.log('Success connected!');
+  },
+  (err) => {
+    console.log(`Failed to connect database ${err}`);
+  },
+);
 
 // console.log(app.get("env"));
 // console.log(process.env);
