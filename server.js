03e71cd7700ed552904f6112a8f099be1b647a5f
indrabsudirman <indrabsudirman@gmail.com>
2023-12-09 21:05:22 +0700

    Add mongoose schema and insert sample document

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config({ path: './config.env' });
 //     console.log('Success connected!');
 //   },
 //   (err) => {
-//     console.log(err);
+//     console.log(`Failed to connect database ${err}`);
 //   },
 // );
 
@@ -30,6 +30,47 @@ mongoose.connect(process.env.DB_LOCAL).then(
     console.log(`Failed to connect database ${err}`);
   },
 );
+
+//Create Schema using mongoose
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is mandatory'],
+    unique: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Duration is mandatory'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Difficulty is mandatory'],
+  },
+  rating: {
+    type: Number,
+    default: 4.0,
+  },
+});
+
+//Create model, standart Model must be PascalCase
+const Tour = mongoose.model('Tour', tourSchema);
+
+//(sample) Save Document into collections MongoDB
+const testTour = new Tour({
+  name: 'The real tour you will forgotten',
+  duration: 4,
+  difficulty: 'medium',
+  rating: 4.9,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error while save document 🧨 :', err);
+  });
 
 // console.log(app.get("env"));
 // console.log(process.env);
